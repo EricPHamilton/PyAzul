21a812dc33135812d00996f42b04a7767b1630b5
Eric Hamilton <ehamilton13@gmail.com>
2020-07-12 16:12:13 -0500

    Add Bag.

--- azul/AzulLogic.py
+++ azul/AzulLogic.py
@@ -1,11 +1,15 @@
 from .Player import Player
+from .TileCollection import TileCollection
 
 class Board():
     def __init__(self):
         self.player1 = Player()
         self.player2 = Player()
+        self.bag = TileCollection(20, 20, 20, 20, 20, 0)
 
     def display(self):
+        self.bag.display()
+        print()
         self.player1.display()
         print()
         self.player2.display()
--- azul/TileCollection.py
+++ azul/TileCollection.py
@@ -1,3 +1,7 @@
 class TileCollection():
     def __init__(self, numBlue, numYellow, numRed, numBlack, numCyan, numWhite):
         self.tiles = [numBlue, numYellow, numRed, numBlack, numCyan, numWhite]
+
+    def display(self):
+        print("BAG:")
+        print(self.tiles)
