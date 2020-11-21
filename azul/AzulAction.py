1a4b40379b255abe4797caa82a78a407dcfdd37c
Eric Hamilton <ehamilton13@gmail.com>
2020-11-20 19:29:32 -0600

    Minor refactoring and documentation

--- azul/AzulAction.py
+++ azul/AzulAction.py
@@ -1,34 +1,39 @@
 from .TileColor import TileColor
 
 class AzulAction:
-    def __init__(self, playerID: id, source, color: TileColor, line):
+    # playerID: ID of the player taking the action
+    # source: Source of the tiles chosen. 0-4 = factories, 5 = center.
+    # color: Color of the tiles chosen.
+    # dest: Destination of the tiles. 0-4 = player lines. 5 = floor line.
+    def __init__(self, playerID: id, source, color: TileColor, dest):
         self.playerID = playerID
         self.source = source
         self.color = color
-        self.line = line
+        self.dest = dest
     
     def toString(self):
-        return str(self.playerID) + ", " + str(self.source) + ", " + str(self.color) + ", " + str(self.line)
+        return str(self.playerID) + ", " + str(self.source) + ", " + str(self.color) + ", " + str(self.dest)
     
     def getActionInt(self) -> int:
         # location = actionInt // 30
         # color = (actionInt % 30) // 6
-        # line = (actionInt % 30) % 6
+        # dest = (actionInt % 30) % 6
         sum = 0
         sum += self.source * 30
         sum += self.color.value * 6
-        sum += int(self.line)
+        sum += int(self.dest)
         return sum
     
+    # Returns a verbal string explaining what took place on this turn.
     def getTurnExplanationString(self) -> str:
         if self.source <= 4:
             source = "factory #" + str(self.source + 1)
         else:
             source = "the center"
         
-        if self.line <= 4:
-            line = "line " + str(self.line + 1)
+        if self.dest <= 4:
+            dest = "line " + str(self.dest + 1)
         else:
-            line = "the floor line"
+            dest = "the floor line"
 
-        return str(f"Player {self.playerID} took {self.color.name} tiles from {source} and put them on {line}")
+        return str(f"Player {self.playerID} took {self.color.name} tiles from {source} and put them on {dest}")
